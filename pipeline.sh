5dd9246f1a3db01cae94d51ea3094c09db9bf702
Nzola De Magalhaes <nmmagalh@uci.edu>
2014-10-21 11:20:48 -0700

    added bash script

---
+++ pipeline.sh
@@ -0,0 +1,25 @@
+echo Cleaning Up the Folder
+rm *.txt
+rm *.tmp
+rm *.csv
+
+echo Download data
+curl https://github.com/zonca/swcarpentry-workshop-pandas/blob/master/rawdata/rawdata.zip?raw=true -o rawdata.zip -L
+
+echo Unpack data and Remove zip File
+unzip rawdata.zip
+rm rawdata.zip
+
+echo Remove empty files such as .tmp
+rm *tmp
+
+echo Rename txt files to csv files
+for f in *.txt
+do
+	mv $f ${f/txt/csv}
+done
+
+echo Available files. File listing
+ls *.csv
+
+
