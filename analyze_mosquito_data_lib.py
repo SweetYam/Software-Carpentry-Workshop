bc3d6561c54a955b5b12bde0470ad60f8d1aef04
Nzola De Magalhaes <nmmagalh@uci.edu>
2014-10-21 13:48:01 -0700

    added changes

--- analyze_mosquito_data_lib.py
+++ analyze_mosquito_data_lib.py
@@ -2,6 +2,7 @@ import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
+""" convert fahrenheit to Celsius"""
 def analyze(data):
     regr_results = sm.OLS.from_formula('mosquitos ~ temperature + rainfall', data).fit()
     parameters = regr_results.params
@@ -12,6 +13,7 @@ def analyze(data):
     plt.show()
     return parameters
 	
+""" Perform regression analysis on mosquito data"""
 def fahr_to_celsius(temperature_fahr):
     temperature_celsius = (temperature_fahr - 32) * 5 / 9.0
     return temperature_celsius
--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -8,8 +8,15 @@ data = pd.read_csv(filename)
 
 # convert temperature data in A1_mosquito_data.csv file,
 # to fahr using fahr_to_celsius function from mosquito_lib file
-data["temperature"] = mostquito_lib.fahr_to_celsius(data["temperature"])
+data["temperature"] = mosquito_lib.fahr_to_celsius(data["temperature"])
+
 
 print data.head()
+
+parameters = mosquito_lib.analyze(data)
+
+#instead of printing parameters via "print parameters", instead we save in a new file
+#Save parameters to file
+parameters.to_csv("parameters.csv")
 
 
